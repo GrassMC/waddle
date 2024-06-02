caf74d86d7bad561080bf8debc83dc2a1bcca749
Nguyễn Thanh Tân <tozydev@nguyenthanhtan.id.vn>
2024-06-02 15:00:52 +0700

    build: refactor project structure (#40)

---
+++ gradlePlugin/build.gradle.kts
@@ -0,0 +1,68 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    `kotlin-dsl`
+    alias(libs.plugins.maven.publish)
+    alias(libs.plugins.plugin.publish)
+}
+
+dependencies {
+    implementation(libs.foojay.resolver)
+    implementation(libs.kotlin.gradle.plugin)
+    implementation(libs.run.task)
+    implementation(libs.paperweight.userdev)
+    implementation(libs.shadow)
+}
+
+val jdkVersion = 17
+kotlin {
+    jvmToolchain {
+        languageVersion = JavaLanguageVersion.of(jdkVersion)
+        vendor = JvmVendorSpec.AZUL
+    }
+    compilerOptions {
+        jvmTarget = JvmTarget.fromTarget(jdkVersion.toString())
+    }
+}
+
+gradlePlugin {
+    plugins {
+        register("waddleSettings") {
+            id = "io.github.grassmc.waddle"
+            implementationClass = "io.github.grassmc.waddle.settings.WaddleSettingsPlugin"
+        }
+        register("waddleJava") {
+            id = "io.github.grassmc.waddle-java"
+            implementationClass = "io.github.grassmc.waddle.java.WaddleJavaPlugin"
+        }
+        register("waddleKotlin") {
+            id = "io.github.grassmc.waddle-kotlin"
+            implementationClass = "io.github.grassmc.waddle.kotlin.WaddleKotlinPlugin"
+        }
+        register("waddlePaper") {
+            id = "io.github.grassmc.waddle-paper"
+            implementationClass = "io.github.grassmc.waddle.paper.WaddlePaperPlugin"
+        }
+        register("waddlePaperInternal") {
+            id = "io.github.grassmc.waddle-paper.internal"
+            implementationClass = "io.github.grassmc.waddle.paper.WaddlePaperInternalPlugin"
+        }
+        register("waddleShadow") {
+            id = "io.github.grassmc.waddle-shadow"
+            implementationClass = "io.github.grassmc.waddle.shadow.WaddleShadowPlugin"
+        }
+    }
+}
+
+publishing {
+    repositories {
+        maven("https://maven.nguyenthanhtan.id.vn/releases") {
+            name = "tozydevReleases"
+            credentials(PasswordCredentials::class)
+        }
+    }
+}
+
+mavenPublishing {
+    signAllPublications()
+}
