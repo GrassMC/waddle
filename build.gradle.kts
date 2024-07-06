019a5bca98f17426246b6c83fd07376724820b91
tozydev <tozydev@nguyenthanhtan.id.vn>
2024-07-06 12:47:54 +0700

    feat: add kotlin version in waddle versions catalog

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,5 @@ plugins {
     `kotlin-dsl` apply false
     alias(libs.plugins.maven.publish) apply false
     alias(libs.plugins.plugin.publish) apply false
+    alias(libs.plugins.buildconfig) apply false
 }
